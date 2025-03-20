4ef6a0e82ce0ea90c6a21be8ec34fd7133de1149
haram-jo <harramaram@gmail.com>
2025-03-20 13:24:41 +0900

    메인화면 설정

--- App.tsx
+++ App.tsx
@@ -1,12 +1,45 @@
+// 리액트 관련 기본 모듈
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import SignupScreen from './screens/SignupScreen';
 import BottomNav from './navigation/BottomNav';
-import { name as appName } from './app.json';
 
-const App = () => {
-  return <BottomNav />;
+export type RootStackParamList = {
+  Home: undefined;
+  Signup: undefined;
+  BottomNav: undefined; // 
+
 };
 
-AppRegistry.registerComponent(appName, () => App);
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        
+        {/* 메인 화면으로 HomeScreen 보여주기 */}
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }} // 상단 헤더를 숨김 처리
+        />
+
+        {/* 회원가입 화면으로 SignupScreen 컴포넌트를 보여주기 */}
+        <Stack.Screen
+          name="Signup"
+          component={SignupScreen}
+          options={{ title: '회원가입' }} // 상단 헤더 제목 설정
+        />
+
+        {/* 기존에 있던 BottomNav(하단 탭 네비게이션) 화면을 추가하고 싶으면 이렇게 추가해도 됨 */}
+        {/* <Stack.Screen name="BottomNav" component={BottomNav} /> */}
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default App;
