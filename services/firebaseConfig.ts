3cbb0c81f8bacac8961982f8eaf632826d4589f9
haram-jo <harramaram@gmail.com>
2025-03-25 12:27:33 +0900

    회원가입 로그인에 맞게 수정

--- services/firebaseConfig.ts
+++ services/firebaseConfig.ts
@@ -1,22 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Firebase 초기 설정 파일(Firebase Console에서 확인 가능)
+// 🔹 Firebase 설정값
 const firebaseConfig = {
-    apiKey: "AIzaSyCiorzdOWoeLPptWAV7xm56TmdN8lLqL4c",
-    authDomain: "iot7-435d1.firebaseapp.com",
-    projectId: "iot7-435d1",
-    storageBucket: "iot7-435d1.firebasestorage.app",
-    messagingSenderId: "981339430588",
-    appId: "1:981339430588:web:7b265ed5b76d038c7afab9",
-    
-
-
+  apiKey: "AIzaSyCiorzdOWoeLPptWAV7xm56TmdN8lLqL4c",
+  authDomain: "iot7-435d1.firebaseapp.com",
+  projectId: "iot7-435d1",
+  storageBucket: "iot7-435d1.appspot.com",
+  messagingSenderId: "981339430588",
+  appId: "1:981339430588:web:7b265ed5b76d038c7afab9",
 };
 
-// Firebase 초기화
+// Firebase 앱 초기화
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 
-
-export { auth};
+// Firebase 인증 인스턴스 내보내기
+export const auth = getAuth(app);
