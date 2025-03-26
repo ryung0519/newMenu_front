2ba4f7020b4044ed4a31792ca0f822cbbe500b46
ryung0519 <happy79891@gmail.com>
2025-03-26 16:36:04 +0900

    readme 변경

--- screens/ProductDetailScreen.tsx
+++ screens/ProductDetailScreen.tsx
@@ -1,17 +1,14 @@
 import React from "react";
-import { ScrollView, View } from "react-native";
+import { ScrollView, Touchable, TouchableOpacity, View } from "react-native";
 import { Text } from "react-native-paper";
 import GlobalStyles from "../styles/GlobalStyles";
-import ProductInfo from "../components/ProductInfo";
 import Review from "../components/Review";
-import RelatedItems from "../components/RelatedItems";
 
 const ProductDetailScreen = () => {
     return (
         <ScrollView style={GlobalStyles.container}>
-            <ProductInfo/>
+                
             <Review/>
-            <RelatedItems/>
             <View>
                 <Text> 상품 상세정보</Text>
             </View>
