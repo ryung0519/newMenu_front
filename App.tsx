8d2a7131da97bdd1bb55ff8691c41d2a5bcf7d38
haram-jo <harramaram@gmail.com>
2025-03-31 18:39:27 +0900

    검색후 결과창 나오는 코드 추가

--- App.tsx
+++ App.tsx
@@ -3,10 +3,16 @@ import React from 'react';
 import MainStack from './navigation/MainStack';
 import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
+import { NavigationContainer } from '@react-navigation/native'; 
 
 const App = () => {
-  return <MainStack />;
+  return (
+    <NavigationContainer> {/* ✅ 내비게이션 컨테이너로 감싸기 */}
+      <MainStack />
+    </NavigationContainer>
+  );
 };
+
 AppRegistry.registerComponent(appName, () => App);
 
 export default App;
