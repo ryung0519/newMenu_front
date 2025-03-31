87f8ffe39739ccc42f57de1b948a57f418af73ad
haram-jo <harramaram@gmail.com>
2025-03-31 18:38:35 +0900

    검색후 결과창 나오는 코드 추가

--- navigation/MainStack.tsx
+++ navigation/MainStack.tsx
@@ -1,32 +1,24 @@
 import React from 'react';
-import ProductDetailScreen from '../screens/ProductDetailScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import BottomNav from './BottomNav';
 import SignupScreen from '../screens/SignupScreen';
 import LoginScreen from '../screens/LoginScreen';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
+import ProductDetailScreen from '../screens/ProductDetailScreen';
+import SearchResultScreen from '../screens/SearchResultScreen';
 
-export type RootStackParamList = {
-    Login: undefined;
-    Signup: undefined;
-    BottomNav: undefined;
-    Product: undefined;
-  };
-
-const Stack = createNativeStackNavigator<RootStackParamList>();
+const Stack = createNativeStackNavigator();
 
 const MainStack = () => {
-    return (
-        <NavigationContainer>
-            {/* 혜령 id={undefined} 추가 및 app.tsx의 내용 추가가*/}
-            <Stack.Navigator   id={undefined} initialRouteName="Login" screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="Login" component={LoginScreen} />
-                <Stack.Screen name="Signup" component={SignupScreen} />
-                <Stack.Screen name="BottomNav" component={BottomNav} />
-                <Stack.Screen name="Product" component={ProductDetailScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="BottomNav" component={BottomNav} />
+      <Stack.Screen name="Signup" component={SignupScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="ProductDetail" component={ProductDetailScreen} />
+      <Stack.Screen name="SearchResult" component={SearchResultScreen} /> 
+   
+    </Stack.Navigator>
+  );
 };
 
 export default MainStack;
