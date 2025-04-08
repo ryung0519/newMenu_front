6e8202070da10f4fc21d0113f15fafb49375be21
haram-jo <harramaram@gmail.com>
2025-04-08 16:21:17 +0900

    자동로그인 및 로그인여부에 따라 메인화면에서 버튼 다르게 보여주기

--- App.tsx
+++ App.tsx
@@ -4,15 +4,18 @@ import MainStack from './navigation/MainStack';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {AuthProvider} from './contexts/AuthContext';
+
+//✅앱이 시작될 때 어떤 화면을 보여줄지 정하는 파일
 
 const App = () => {
   return (
     <GestureHandlerRootView style={{flex: 1}}>
-      <MainStack />
+      <AuthProvider>
+        <MainStack />
+      </AuthProvider>
     </GestureHandlerRootView>
   );
 };
-
-AppRegistry.registerComponent(appName, () => App);
 
 export default App;
