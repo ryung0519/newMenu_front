8b824e63beba05c0bf5820c027412db6473100a0
haram-jo <harramaram@gmail.com>
2025-04-10 20:50:49 +0900

    BrandMenuList 관련 코드 추가

--- navigation/HomeStack.tsx
+++ navigation/HomeStack.tsx
@@ -4,6 +4,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
 import ProductDetailScreen from '../screens/ProductDetailScreen';
 import SearchResultScreen from '../screens/SearchResultScreen';
+import BrandMenuListScreen from '../screens/BrandMenuListScreen';
 
 {
   /* 하단탭 계속 유지하기 위해 새로 만든 파일 */
@@ -20,6 +21,8 @@ const HomeStack = () => {
       <Stack.Screen name="Product" component={ProductDetailScreen} />
       <Stack.Screen name="SearchResult" component={SearchResultScreen} />
       <Stack.Screen name="ProductDetail" component={ProductDetailScreen} />
+      <Stack.Screen name="BrandMenuList" component={BrandMenuListScreen} />
+
       {/* 여기에 위에 같은 형식으로 추가하면됨*/}
     </Stack.Navigator>
   );
