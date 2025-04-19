07866f2d3b2f959373badd4086383c5fb413d6e7
KangGuYong <rivenine123@naver.com>
2025-04-19 16:22:31 +0900

    리뷰코드 리팩터링

--- components/review/Review.tsx
+++ components/review/Review.tsx
@@ -1,5 +1,5 @@
 import {Text, View} from 'react-native';
-import GlobalStyles from '../styles/GlobalStyles';
+import GlobalStyles from '../../styles/GlobalStyles';
 import React from 'react';
 
 const Review = () => {
